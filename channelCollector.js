be3c1cccbcbf7eb4f681796b33b9ccbad10daf65
JongAm Oh <shinemuphy@gmail.com>
2015-04-13 14:31:14 +0900

    get program list

--- channelCollector.js
+++ channelCollector.js
@@ -1,69 +1,67 @@
 var cheerio = require('cheerio');
 var request = require('request');
-
+var fs = require('fs');
 
 function process() {
-    var url = 'http://movie.daum.net/tv/guide/ranking/programRanking.do?type=3&page=1';
-    request(url, function(error, response, html) {
-      if (error) {
-        throw error
-      };
-      // console.log(html);
-      var $ = cheerio.load(html);
+        var options = {
+            url: 'http://tvguide.naver.com',
+            headers: {
+                "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36",
+                //"Accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
+            }
+        };
+        request(options, function(error, response, html) {
+            if (error) {
+                throw error
+            };
+            //console.log(html);
+            var b = html.match(/PROGRAM_SCHEDULES=[\s\S]*?;/gm);
+            var strSchedule = b[0].replace('PROGRAM_SCHEDULES=','').replace(';','');
+            console.log(strSchedule);
+            var schedule = JSON.parse(strSchedule);
+            console.log(schedule.channelList.length);
+            fs.writeFile("./tmp.txt", strSchedule, function(err) {
+                if (err) {
+                    return console.log(err);
+                }
 
-      $('#popularProgram tbody tr').each(function(idx) {
-        var $tr = $(this);
-        var $thumbnailImg = $tr.find('img.poster');
-        if ($thumbnailImg.length > 0) {
-          var thumbnailImgUrl = $thumbnailImg.attr('src').replace('C38x55', 'C198x288');
-          var title = $thumbnailImg.attr('alt');
-          console.log(title);
-          var castName = $tr.find('td.td3 span.cast').text();
-          console.log(castName);
-          var roomInfo = {
-            id: idx,
-            thumbnailImgUrl: thumbnailImgUrl,
-            title: title,
-            castName: castName
-          };
-          insertRoomInfo(roomInfo);
-        }
+                console.log("The file was saved!");
+            });
+            //var PROGRAM_SCHEDULES = JSON.parse(b[1]);
+        });
 
-      })
-    });
-
-  }
-  // var SECRET_KEY = 'aN6e5tYi9pxsgYaF7relprBvdTxutjT35pGQz7BR';
-  // var urls = ['http://movie.daum.net/tv/guide/ranking/programRanking.do?type=3&page=1'];
+    }
+    // var SECRET_KEY = 'aN6e5tYi9pxsgYaF7relprBvdTxutjT35pGQz7BR';
+    // var urls = ['http://movie.daum.net/tv/guide/ranking/programRanking.do?type=3&page=1'];
 
 
 function insertRoomInfo(roomInfo) {
-  var options = {
-    uri: 'https://sizzling-heat-271.firebaseio.com/rooms.json',
-    method: 'POST',
-    json: roomInfo
-  };
+    var options = {
+        uri: 'https://sizzling-heat-271.firebaseio.com/rooms.json',
+        method: 'POST',
+        json: roomInfo
+    };
 
-  request(options, function(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      console.log(body.id) // Print the shortened url.
-    }
-  });
+    request(options, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body.id) // Print the shortened url.
+        }
+    });
 }
 
 function deleteRoomInfo() {
-  var options = {
-    uri: 'https://sizzling-heat-271.firebaseio.com/rooms.json',
-    method: 'DELETE'
-  };
+        var options = {
+            uri: 'https://sizzling-heat-271.firebaseio.com/rooms.json',
+            method: 'DELETE'
+        };
 
-  request(options, function(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      console.log(body.id) // Print the shortened url.
+        request(options, function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log(body.id) // Print the shortened url.
+            }
+        });
     }
-  });
-}
-// deleteRoomInfo();
+    // deleteRoomInfo();
 process();
 // console.log('test replace'.replace('test','text'));
 // deleteRoomInfo();
